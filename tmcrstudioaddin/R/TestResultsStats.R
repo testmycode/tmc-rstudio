e15552c365f36fed25606ff58cfd6c98c03ece16
samuvait <samu.vaittinen@helsinki.fi>
2017-11-03 09:58:58 +0200

    Added a function to format the submission json data to the result format

--- tmcrstudioaddin/R/TestResultsStats.R
+++ tmcrstudioaddin/R/TestResultsStats.R
@@ -1,3 +1,5 @@
+library(jsonlite)
+
 .testsPassedPercentage <- function(testResults) {
   if (length(testResults) == 0) return("100%")
 
@@ -10,4 +12,25 @@
 
   passedPercentage <- round(passed / length(testResults), digit = 2)
   return(paste(sep = "", toString(passedPercentage * 100), "%"))
+}
+
+processSubmissionJson <- function(json) {
+  output <- fromJSON(json, simplifyVector = FALSE)
+  exercises <- list()
+  for (submission in output$submissions) {
+    result <- list()
+    result[["name"]] <- submission$exercise_name
+    result[["status"]] <- .getStatusFromBoolean(submission$all_tests_passed)
+    result[["message"]] <- ""
+    exercises[[length(exercises) + 1]]  <- result
+  }
+  return(exercises)
+}
+
+.getStatusFromBoolean <- function(bol) {
+  status <- "fail"
+  if (bol) {
+    status <- "pass"
+  }
+  return(status)
 }
