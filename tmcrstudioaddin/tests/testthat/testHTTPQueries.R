ad98518ce9d20715ecb3a0828bbfab5d8c300216
Aleksis Vuoksenmaa <aleksis.vuoksenmaa@helsinki.fi>
2017-11-09 13:10:12 +0200

    Added test to test HTTPQueries.R

--- tmcrstudioaddin/tests/testthat/testHTTPQueries.R
+++ tmcrstudioaddin/tests/testthat/testHTTPQueries.R
@@ -1,8 +1,20 @@
 #TODO Fix this
 test_that("Exercises are downloaded correctly", {
   token <- authenticate("rtest", "asdasdasd","https://tmc.mooc.fi")
-  download_exercise(token = token,
-                     exercise_id = "36463",
-                     exercise_directory = "resources/downloaded_exercises/")
-  expect_true(TRUE)
+  download_all_exercises(token = token,
+                     exercise_id = "242")
+
+  course_name <- "hy-C-programming-Open"
+  user_home <- Sys.getenv("HOME")
+  r_home <- file.path(user_home, "tmcr-projects", fsep = .Platform$file.sep)
+
+
+
+                     # The path where we want to download the exercises.
+  course_directory_path <- file.path(r_home, course_name,
+                        fsep = .Platform$file.sep)
+
+  expect_true(dir.exists(course_directory_path))
+
+
 })
