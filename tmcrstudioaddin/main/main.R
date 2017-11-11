334ebe4e6a53ca094f6ef50014516772005854ac
eerojala <eero.ojala@helsinki.fi>
2017-11-11 20:28:05 +0200

    upload_current_exercise now reads exercise id from metadata

--- tmcrstudioaddin/main/main.R
+++ tmcrstudioaddin/main/main.R
@@ -1,5 +1,8 @@
 source("R/Authentication.R")
+source("R/Zipper.R")
+source("R/HTTPQueries.R")
 
-token <- authenticate(username = "usernameHere", password = "passwordHere")
-course <- tmcrstudioaddin::temp_get_course(token = token)
-
+token <- tmcrstudioaddin::authenticate(username = "rtest", password = "asdasdasd", serverAddress = "https://tmc.mooc.fi")
+path <- paste(sep = "", getwd(), "/hello_world")
+setwd(path)
+status <- upload_current_exercise(token = token)
