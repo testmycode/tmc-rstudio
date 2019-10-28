ce0885aee626f5063e4715751fe46358ab929703
Petteri Piiroinen <petteri.piiroinen@gmail.com>
2019-10-28 07:18:00 +0200

    Cleanup 30

--- tmcrstudioaddin/R/TMC_plugin.R
+++ tmcrstudioaddin/R/TMC_plugin.R
@@ -7,8 +7,9 @@
 #'
 #' @usage tmcGadget()
 #'
-#' @details The TMC \code{RStudio} addin was made using \code{\link[shiny]{shiny-package}}, which
-#' allows making web applications and \code{RStudio} addins using \code{R}.
+#' @details The TMC \code{RStudio} addin was made using
+#' \code{\link[shiny]{shiny-package}}, which allows making web
+#' applications and \code{RStudio} addins using \code{R}.
 tmcGadget <- function() {
   # Assign the UI_disabled variable as a global variable
   assign(x = "UI_disabled", value = FALSE, envir = .GlobalEnv)
@@ -16,8 +17,10 @@ tmcGadget <- function() {
   ui <- miniPage(
     shinyjs::useShinyjs(),
 
-    gadgetTitleBar(title = "TMC RStudio", right = NULL,
-                   left = miniTitleBarCancelButton(inputId = "exit", label = "Exit")),
+    gadgetTitleBar(title = "TMC RStudio",
+                   right = NULL,
+                   left = miniTitleBarCancelButton(inputId = "exit",
+                                                   label = "Exit")),
 
     miniTabstripPanel(
       .loginTabUI(id = "login"),
@@ -28,18 +31,24 @@ tmcGadget <- function() {
 
   server <- function(input, output, session) {
 
-    globalReactiveValues <- reactiveValues(credentials = tmcrstudioaddin::getCredentials(), downloadedExercises = downloadedExercisesPaths(),
-                                           exerciseMap = list(), downloadedExercisesMap = list())
+    globalReactiveValues <-
+      reactiveValues(credentials = tmcrstudioaddin::getCredentials(),
+                     downloadedExercises = downloadedExercisesPaths(),
+                     exerciseMap = list(),
+                     downloadedExercisesMap = list())
     # Function for the exit button
     observeEvent(input$exit, {
-      if(UI_disabled) return()
+      if (UI_disabled) return()
 
       return(shiny::stopApp())
     })
 
-    shiny::callModule(.loginTab, "login", globalReactiveValues = globalReactiveValues)
-    shiny::callModule(.courseTab, "courses", globalReactiveValues = globalReactiveValues)
-    shiny::callModule(.submitTab, "testAndSubmit", globalReactiveValues = globalReactiveValues)
+    shiny::callModule(.loginTab, "login",
+                      globalReactiveValues = globalReactiveValues)
+    shiny::callModule(.courseTab, "courses",
+                      globalReactiveValues = globalReactiveValues)
+    shiny::callModule(.submitTab, "testAndSubmit",
+                      globalReactiveValues = globalReactiveValues)
   }
 
   shiny::runGadget(app = ui, server = server)
