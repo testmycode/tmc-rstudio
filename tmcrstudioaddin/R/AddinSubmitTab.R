a92ca2b277fc78b63ba7135a59e20f06e916d150
Petteri Piiroinen <petteri.piiroinen@gmail.com>
2020-07-24 09:18:33 +0300

    Fixing server messages 1

--- tmcrstudioaddin/R/AddinSubmitTab.R
+++ tmcrstudioaddin/R/AddinSubmitTab.R
@@ -93,8 +93,50 @@
     tmcrstudioaddin::enable_submit_tab()
   })
 
-  submitExercise <- observeEvent(input$submit, {
+  submitExercise <- observeEvent(input$submit, { 
     if (UI_disabled) return()
+
+    tranlation_df <- 
+      as.data.frame(
+        stringsAsFactors = FALSE,
+        matrix(byrow = TRUE, ncol = 2,
+               c("1 is",         "1 is solved correctly",
+                 "1a and",       "1a and 1b are solved correctly",
+                 "1a is",        "1a is solved correctly",
+                 "1a, 1b",       "1a, 1b and 1c are solved correctly",
+                 "1b and",       "1b and 1c are solved correctly",
+                 "1b is",        "1b is solved correctly",
+                 "1c and",       "1c and 1d is solved correctly",
+                 "1c is",        "1c is solved correctly",
+                 "1d and",       "1d and 1e are solved correctly",
+                 "2 is",         "2 is solved correctly",
+                 "2a and",       "2a and 2b are solved correctly",
+                 "2a is",        "2a is solved correctly",
+                 "2a, 2b",       "2a, 2b and 2c are solved correctly",
+                 "2b and",       "2b and 2c are solved correctly",
+                 "2b is",        "2b is solved correctly",
+                 "2c and",       "2c and 2d are solved correctly",
+                 "2c is",        "2c is solved correctly",
+                 "2c, 2d",       "2c, 2d and 2e are solved correctly",
+                 "2c, and",      "2c, and 2d are solved correctly",
+                 "2d and",       "2d and 2e are solved correctly",
+                 "2d is",        "2d is solved correctly",
+                 "3 is",         "3 is solved correctly",
+                 "3a is",        "3a is solved correctly",
+                 "3b and",       "3b and 3c are solved correctly",
+                 "3b is",        "3b is solved correctly",
+                 "4a and",       "4a and 4b are solved correctly",
+                 "4a is",        "4a is solved correctly",
+                 "4b is",        "4b is solved correctly",
+                 "4c is",        "4c is solved correctly",
+                 "exercise 3b",  "exercise 3b is solved correctly",
+                 "exercise 3c",  "exercise 3c is solved correctly",
+                 "exercises 1a", "exercises 1a and 1b are solved correctly",
+                 "exercises 1c", "exercises 1c and 1d are solved correctly",
+                 "exercises 2a", "exercises 2a and 2b are solved correctly",
+                 "exercises 2c", "exercises 2c and 2d are solved correctly",
+                 "exercises 3a", "exercises 3a and 3b is solved correctly")))
+    names(tranlation_df) <- c("key", "translation")
 
     tmcrstudioaddin::disable_submit_tab()
     submitRes <- NULL
@@ -110,6 +152,46 @@
       submitRes <- submitExercise(selectedExercisePath) })
     }
     if (is.null(submitRes$error)) {
+      if (!is.null(reactive$testResults)) {
+        test_names_local  <- c(sapply(X = reactive$testResults, function(x) x$name),
+                               "")
+        test_names_server <- sapply(X = submitRes$data$tests, function(x) x$name)
+        ind1 <- 1
+        for (ind2 in seq_along(test_names_server)) {
+          lname <- test_names_local[ind1]
+          sname <- test_names_server[ind2]
+          .ddprint(ind1)
+          .ddprint(ind2)
+          .ddprint(lname)
+          .ddprint(sname)
+          .ddprint(substr(lname, 1, nchar(sname)) == sname)
+          if (substr(lname, 1, nchar(sname)) == sname) {
+            ind1 <- ind1 + 1
+            test_names_server[ind2] <- lname
+          } else {
+            temp_bool <- tranlation_df$key == sname
+            if (sum(temp_bool)) {
+              .ddprint(which(temp_bool))
+              .ddprint(tranlation_df$translation[which(temp_bool)])
+              test_names_server[ind2] <- tranlation_df$translation[which(temp_bool)]
+            }
+            # pppp
+          }
+        }
+        .ddprint(test_names_local)
+        .ddprint(test_names_server)
+        resolved_tests <-
+          mapply(function(x, name) {
+                   x$name <- name
+                   x
+                 },
+                 submitRes$data$tests,
+                 test_names_server,
+                 SIMPLIFY = FALSE)
+        .ddprint(str(resolved_tests))
+        .ddprint(str(tranlation_df))
+        submitRes$data$tests <- resolved_tests
+      }
       reactive$submitResults <- submitRes$data
       reactive$testResults <- submitRes$data$tests
       reactive$runStatus <- "success"
