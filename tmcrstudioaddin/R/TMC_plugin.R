f98ba89065eefe372ef43dbffd1f608ec53d6ce7
Petteri Piiroinen <petteri.piiroinen@gmail.com>
2021-05-02 17:33:46 +0300

    Version 0.8.4

--- tmcrstudioaddin/R/TMC_plugin.R
+++ tmcrstudioaddin/R/TMC_plugin.R
@@ -175,7 +175,7 @@ tmcGadget <- function() {
 
 .create_rtmc_ui <- function(tabs_data_list) {
   # cat("4\n")
-  css_prefix <- "tmcrstudioaddin-0.8.3"
+  css_prefix <- "tmcrstudioaddin-0.8.4"
   used_theme <- .choose_used_theme(css_prefix)
   shiny::addResourcePath(css_prefix,
                          system.file("www", package = "tmcrstudioaddin"))
